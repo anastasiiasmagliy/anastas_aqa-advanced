3e9ae0c07db9593e38e0be36c03e84cce3fafa3e
Anastasiia Smagliy <anastasiiasmagliy@gmail.com>
2026-09-13 18:28:01 +0300

    +

--- jsBasics/task1.js
+++ jsBasics/task1.js
@@ -1,12 +1,14 @@
 // Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. 
 // Присвойте їм значення та виведіть кожну змінну на консоль.
 
-let hername = "Anastasiia";
-let age = "27";
-let issheagirl = true;
-let 
+const hername = "Anastasiia";
+const age = "27";
+const issheagirl = true;
+const car = null;
+const job; 
 
 console.log (hername);
 console.log (age);
 console.log (issheagirl);
-
+console.log (car);
+console.log (job);
