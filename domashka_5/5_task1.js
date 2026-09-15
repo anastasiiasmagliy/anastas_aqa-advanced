9c8ebefad75880881eab6c5ebfbcb1d4bcd800bb
Anastasiia Smagliy <anastasiiasmagliy@gmail.com>
2026-09-15 22:40:46 +0300

    +

--- domashka_5/5_task1.js
+++ domashka_5/5_task1.js
@@ -7,3 +7,20 @@
 // Якщо середня оцінка від 71 до 80 (включно), вивести "Добре".
 // Якщо середня оцінка від 81 до 90 (включно), вивести "Дуже добре".
 // Якщо середня оцінка від 91 до 100 (включно), вивести "Відмінно".
+
+let averageGrade = 81;
+if (averageGrade < 60) {
+console.log ("Незадовільно");
+}
+else if (averageGrade >= 60 && averageGrade <=70) {
+console.log ("Задовільно");
+}
+else if (averageGrade >= 71 && averageGrade <=80) {
+console.log ("Добре");
+}
+else if (averageGrade >= 81 && averageGrade <=90) {
+console.log ("Дуже добре");
+}
+else if (averageGrade >= 91 && averageGrade <=100) {
+console.log ("Відмінно");
+}
